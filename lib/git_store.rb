20b9acbda2026fdf62416902b37613f3cb13e26c
Matthias Georgi <matti.georgi@gmail.com>
2008-12-19 18:00:26 +0100

    Monkey patched Hash#to_yaml. Hashes now serialized as sorted maps.

--- lib/git_store.rb
+++ lib/git_store.rb
@@ -1,5 +1,18 @@
 require 'grit'
 
+# This fix ensures sorted yaml maps.
+class Hash
+	def to_yaml( opts = {} )
+		YAML::quick_emit( object_id, opts ) do |out|
+      out.map( taguri, to_yaml_style ) do |map|
+        sort_by { |k, v| k.to_s }.each do |k, v|
+          map.add( k, v )
+        end
+      end
+    end
+	end
+end
+
 class GitStore
 
   class DefaultHandler
