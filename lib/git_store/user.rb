e6b66355661fa67cdffc73a0264034c2349a16ca
Michael Fellinger <m.fellinger@gmail.com>
2009-05-31 16:31:50 +0900

    Make sure User::from_config fails with useful exception in case there is no user information in the git config

--- lib/git_store/user.rb
+++ lib/git_store/user.rb
@@ -9,7 +9,24 @@ class GitStore
       "#<GitStore::User name=%p email=%p time=%p>" % [name, email, time]
     end
 
+    def self.from_config
+      name, email = config_get('user.name'), config_get('user.email')
       new name, email, Time.now
+    end
+
+    def self.config_get(key)
+      value = `git config #{key}`.chomp
+
+      if $?.exitstatus == 0
+        return value unless value.empty?
+        raise RuntimError, "#{key} is empty"
+      else
+        raise RuntimError, "No #{key} found in git config"
+      end
+    end
+
+    def self.config_user_email
+      email = `git config user.email`.chomp
     end
 
     def self.parse(user)
@@ -17,7 +34,5 @@ class GitStore
         new match[1].strip, match[2].strip, Time.at(match[3].to_i + match[4].to_i * 3600)
       end
     end
-
   end
-
 end
